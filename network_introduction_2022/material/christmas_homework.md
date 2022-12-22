da10f81ddbd9d3bc5f5d9712993e0bf87639263d
PacMat99 <pacchinmattia@gmail.com>
2022-12-22 11:33:40 +0100

    adding christmas homework

---
+++ network_introduction_2022/material/christmas_homework.md
@@ -0,0 +1,13 @@
+# Christmas Homework 2022
+
+## Consegna
+
+Per le vacanze di Natale dovrete fare una ricerca che consisterà in un approfondimento su alcuni argomenti che abbiamo già visto o argomenti che non vedremo a lezione.
+
+Vi chiedo di preparare una presentazione per la classe e di fare il lavoro in gruppi di 3-5 persone.
+Ogni gruppo dovrà scegliere un argomento differente, pertanto mettetevi d'accordo tra voi
+
+## Argomenti disponibili
+
+1. Cos'è e a cosa serve una VPN? 
+2. CSMA
--- network_introduction_2022/webapp/app.py
+++ network_introduction_2022/webapp/app.py
@@ -1,22 +1,22 @@
-from flask import Flask, render_template, request#, flash
-#from os import urandom
+from flask import Flask, render_template, request, flash
+from os import urandom
 
 app = Flask(__name__)
 
-#app.secret_key = urandom(24).hex()
+app.secret_key = urandom(24).hex()
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
 	if request.method == 'POST':
 		secret_message = request.form['secret_message']
 
-		#if not secret_message:
-		#	flash('You must send a message!')
-		#else:
-		#	flash('Message correctly sent!')
-		print(secret_message) # indent this line for https
+		if not secret_message:
+			flash('You must send a message!')
+		else:
+			flash('Message correctly sent!')
+			print(secret_message) # indent this line for https
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	#app.run(debug=True, host='0.0.0.0', ssl_context='adhoc', port=13800)
-	app.run(debug=True, host='0.0.0.0', port=8080)
+	app.run(debug=True, host='0.0.0.0', ssl_context='adhoc', port=8080)
+	#app.run(debug=True, host='0.0.0.0', port=8080)
