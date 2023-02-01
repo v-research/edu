a5599c2914ac96b02117d43e66894b574dd2126c
PacMat99 <pacchinmattia@gmail.com>
2023-02-02 00:24:03 +0100

    exam update

--- network_introduction_2022/exam/exam.md
+++ network_introduction_2022/exam/exam.md
@@ -4,7 +4,7 @@
 
 - Viene fornito in allegato un file python da completare con pezzi mancanti. 
 - Non modificare l'intestazione delle funzioni e i parametri che vengono passati alle stesse.
-- Per il funzionamento del comando ifconfig deve
+- Per il funzionamento del comando ifconfig deve essere installata la libreria net-tools
 - Per eseguire il file python utlizzare il comando
 	~~~
 	python3 *nome_file.py*
@@ -12,7 +12,9 @@
 
 ## Esercizi
 
-1. 
+### check_internet_connections
+
+Restituire un bool che indica se dal nostro terminale riusciamo a raggiungere google.com.
 
 ## Note
 
@@ -28,3 +30,8 @@ subprocess.run('cmd', shell=True)
 subprocess.check_output('cmd', shell=True)
 
 La funzione subprocess.run restituisce un bool che indica se la funzione lanciata è andata o meno a buon fine
+
+Per limitare il numero di pacchetti inviati dal ping, lanciare il comando con i parametri -c 21 dove 21 indica il numero di pacchetti che vogliamo inviare:
+	~~~
+	ping -c 21 ciao.it
+	~~~
--- network_introduction_2022/exam/solutions.py
+++ network_introduction_2022/exam/solutions.py
@@ -5,7 +5,7 @@ def create_file(filename):
 	subprocess.run(['touch', filename])
 
 def give_permissions(filename):
-	subprocess.run(['chmod', '777', 'ciao'])
+	subprocess.run(['chmod', '777', filename])
 
 def check_internet_connection():
 	# can we connect to the internet?
@@ -14,34 +14,92 @@ def check_internet_connection():
 def get_ip_info():
 	# get network information with "ifconfig" command and save them in a file
 	# net-tools lib should be installed with atp for using ifconfig
-	lan_info = subprocess.check_output(['ifconfig']).decode('utf-8')
-	inet = re.findall(r'inet [0-9].*', lan_info)
-	print(inet)
+	ifconfig_out = subprocess.check_output(['ifconfig']).decode('utf-8')
+	info = re.findall(r'inet [0-9].+', ifconfig_out)
+	splitted_info = re.split(r'[\W]+', info[0])
+	print('info:', splitted_info)
+	return splitted_info
 
-def find_network_ip():
+def write_ip_info(info):
+	ip = info[1] + '.' + info[2] + '.' + info[3] + '.' + info[4]
+	write_ip = 'echo "ip: ' + ip + '" > lan_info.txt'
+	subprocess.run(write_ip, shell=True)
+	netmask = info[6] + '.' + info[7] + '.' + info[8] + '.' + info[9]
+	write_netmask = 'echo "netmask: ' + netmask + '" >> lan_info.txt'
+	subprocess.run(write_netmask, shell=True)
+	return [info[1], info[2], info[3], info[4], info[6], info[7], info[8], info[9]]
+
+def bin_to_dec(binary):
+	decimal = 0
+	i = 0
+	while binary != 0:
+		dec = binary % 10
+		decimal = decimal + dec * pow(2, i)
+		binary = binary // 10
+		i += 1
+	return(decimal)
+
+def dec_to_bin(decimal):
+	binary = 0
+	mult = 1
+	while decimal > 0:
+		binary += (decimal % 2) * mult
+		mult *= 10
+		decimal = decimal // 2
+	return(binary)
+
+def size_of(n):
+	i = 0
+	while n > 0:
+		i += 1
+		n = n // 10
+	return i
+
+def write_network_ip(info):
 	# 1. calculate network address starting from our ip and netmask
 	# 2. how many devices can be connected to the network?
 	#file_text = subprocess.check_output(['cat', '...'])
-	#...
-	return
+	
+	ip_bin = ''
+	for count in range(1, 4):
+		n_bin = dec_to_bin(int(info[count]))
+		print('size_of:')
+		print(size_of(n_bin))
+		if size_of(n_bin) < 8:
+			print('size_of:')
+			print(size_of(n_bin))
+			
+			ip_bin += dec_to_bin(int(info[count]))
+			ip_bin += dec_to_bin(int(info[count]))
+
+	'''netmask_bin = ''
+	for  count in range(6, 9):
+		netmask_bin[i] = dec_to_bin(int(info[count + 5]))
+
+	network_dec = []
+	for count in range(0, 3):
+		network_dec = bin_to_dec(network_bin[count])
+
+	network = str(network_dec[0]) + '.' + str(network_dec[1]) + '.' + str(network_dec[2]) + '.' + str(network_dec[3])
+	write_netmask = 'echo "network: ' + network + '" >> lan_info.txt'
+	subprocess.run(write_netmask, shell=True)'''
 
 def hops_to_site(site):
 	# write in a file how many hops to go to the given site
 	file_text = subprocess.check_output(['traceroute', site]).decode('utf-8')
 	print(file_text)
-	
-	#channels = re.findall(r'Channel [0-9]*', output)
 
 def main():
-	create_file('ciao')
-	give_permissions('filename')
+	create_file('lan_info.txt')
+	give_permissions('lan_info.txt')
 	if (check_internet_connection()):
 		print('I can connect to the internet! :D')
 	else:
 		print('I can\'t connect to the internet! :(')
-	get_ip_info()
-	find_network_ip()
-	hops_to_site('google.it')
+	info = get_ip_info()
+	write_ip_info(info)
+	write_network_ip(info)
+	#hops_to_site('google.it')
 
 if __name__ == '__main__':
 	main()
